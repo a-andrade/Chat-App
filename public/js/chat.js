77c535ed4f5d139adeaee09838c411fb2883d91d
Andres Andrade <a.andrade09@yahoo.com>
2021-03-19 12:22:29 -0700

    Added enable/disable features to buttons

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,32 +14,55 @@ const socket = io()
 //     socket.emit('increment')
 // })
 
+// s17.160 \\
+// elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+// challenge
+const $sendLocationButton = document.querySelector('#send-location')
+
 // s17.156 \\
 // challenge
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    // disable
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     //const message = document.querySelector('#message').value
     const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message, (error) => {
+        // s17.160 \\
+        // enable
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         // s17.159 \\
         if (error) {
             return console.log(error)
         }
+
         console.log('Message delivered.')
     })
 })
 
 // s17.158 \\
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
+
+    // challenge
+    // disable
+    $sendLocationButton.setAttribute('disabled', 'disabled')
 
     // challenge
     navigator.geolocation.getCurrentPosition((position) => {
@@ -48,6 +71,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
             longitude: position.coords.longitude
             // s17.159 \\
         }, () => {
+            // enable
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location shared!')
         })
     })
